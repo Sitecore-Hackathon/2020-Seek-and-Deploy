eb4cf8c1578edefdad98f34093b1af4b094b316f
Victor Bauz <vbauz@oshyn.com>
2020-02-29 08:20:37 -0500

    auth

---
+++ src/Feature/Authentication/website/Meetcore.Feature.Authentication/Helper/FieldHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.ExperienceForms.Models;
+
+namespace Meetcore.Feature.Authentication.Helper
+{
+    public static class FieldHelper
+    {
+        public static IViewModel GetFieldById(Guid id, IList<IViewModel> fields)
+        {
+            return fields.FirstOrDefault(f => Guid.Parse(f.ItemId) == id);
+        }
+
+        public static string GetValue(object field)
+        {
+            return field?.GetType().GetProperty("Value")?
+                       .GetValue(field, null)?.ToString() ?? string.Empty;
+        }
+    }
+}
