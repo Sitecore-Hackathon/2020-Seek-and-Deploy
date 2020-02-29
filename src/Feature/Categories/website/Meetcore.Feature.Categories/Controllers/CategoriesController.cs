66317b0fca69da434d157b6004740488fbf74936
Esteban Capello <ecapello@oshyn.com>
2020-02-29 07:41:16 -0500

    Initial commit for categories

---
+++ src/Feature/Categories/website/Meetcore.Feature.Categories/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Web.Mvc;
+using Meetcore.Feature.Categories.Services;
+using Sitecore.Mvc.Presentation;
+
+namespace Meetcore.Feature.Categories.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        protected readonly ICategoryRepository CategoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            Debug.Assert(categoryRepository != null);
+            CategoryRepository = categoryRepository;
+        }
+
+        public ActionResult List()
+        {
+            var catRoot = RenderingContext.Current.ContextItem;
+            var categories = CategoryRepository.GetCategories(catRoot);
+            return View(categories);
+        }
+    }
+}
---
+++ src/Feature/Categories/website/Meetcore.Feature.Categories/Services/CategoryRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Sitecore.Abstractions;
+using Sitecore.ContentSearch;
+using Sitecore.Data.Items;
+
+namespace Meetcore.Feature.Categories.Services
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        protected readonly BaseFactory Factory;
+        protected readonly BaseItemManager ItemManager;
+
+        public CategoryRepository(BaseFactory factory, BaseItemManager itemManager)
+        {
+            Debug.Assert(factory != null);
+            Debug.Assert(itemManager != null);
+            Factory = factory;
+            ItemManager = itemManager;
+        }
+
+        public IEnumerable<Item> GetCategories(Item parent)
+        {
+            using (var context = GetSearchContext(parent))
+            {
+                var results = context.GetQueryable<CategorySearchQuery>()
+                    .Where(evnt => evnt.Paths.Contains(parent.ID) && evnt.Templates.Contains(Templates.Category.Id))
+                    .Select(x => new {
+                        Uri = x.UniqueId,
+                        Database = Factory.GetDatabase(x.UniqueId.DatabaseName)
+                    }).ToList();
+                return results.Select(x => ItemManager.GetItem(x.Uri.ItemID, x.Uri.Language, x.Uri.Version, x.Database));
+            }
+        }
+
+        protected virtual IProviderSearchContext GetSearchContext(Item item)
+        {
+            return ContentSearchManager.GetIndex((SitecoreIndexableItem)item).CreateSearchContext();
+        }
+    }
+}
