24ec703ee875da6f16920bdc49383534d04e24dc
Victor Bauz <vbauz@oshyn.com>
2020-02-29 11:12:45 -0500

    register working

---
+++ src/Feature/Authentication/website/Meetcore.Feature.Authentication/SubmitActions/LoginUser/LoginUserData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Meetcore.Feature.Authentication.SubmitActions.LoginUser
+{
+    public class LoginUserData
+    {
+        public Guid UserNameFieldId { get; set; }
+        public Guid PasswordFieldId { get; set; }
+    }
+}
