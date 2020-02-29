69ccdc0d0f4a23c0726fb237cc7c5918d1bdece2
Cesar Yanez <cyanez@oshyn.com>
2020-02-29 08:01:27 -0500

    BE FE event integration

--- src/Feature/Events/Meetcore.Feature.Events/Controllers/EventsController.cs
+++ src/Feature/Events/Meetcore.Feature.Events/Controllers/EventsController.cs
@@ -1,23 +1,27 @@
 ﻿using System.Diagnostics;
 using System.Web.Mvc;
 using Meetcore.Feature.Events.Services;
+using Sitecore.Abstractions;
 using Sitecore.Mvc.Presentation;
+using Sitecore.Data;
 
 namespace Meetcore.Feature.Events.Controllers
 {
     public class EventsController : Controller
     {
         protected readonly IEventRepository EventRepository;
+        protected readonly BaseItemManager ItemManager;
 
-        public EventsController(IEventRepository eventRepository)
+        public EventsController(IEventRepository eventRepository, BaseItemManager itemManager)
         {
             Debug.Assert(eventRepository != null);
             EventRepository = eventRepository;
+            ItemManager = itemManager;
         }
 
         public ActionResult List()
         {
-            var eventRoot = RenderingContext.Current.ContextItem;
+            var eventRoot = Sitecore.Context.Database.GetItem(RenderingContext.Current.Rendering.DataSource);
             var events = EventRepository.GetEvents(eventRoot);
             return View(events);
         }
